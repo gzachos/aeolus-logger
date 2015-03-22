40966b1951944ca7fd7714299356eda3d1e726e9
George Zachos <cse32439@cs.uoi.gr>
2015-03-22 12:16:43 +0200

    Add copyright to update_data.sh and generate_input_data.sh

--- full_html_output_website/scripts/generate_input_data.sh
+++ full_html_output_website/scripts/generate_input_data.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+#+-----------------------------------------------------------------------+
+#|                 Copyright (C) 2015 George Z. Zachos                   |
+#+-----------------------------------------------------------------------+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Contact Information:
+# Name: George Zachos
+# Email: gzzachos_at_gmail.com
+
+
+
 function retrieve_data () {
 	wget http://192.168.254.$1/index.html -O /var/www/html/emerson_$2/data/main.txt
 	wget http://192.168.254.$1/tmp/1102.xml -O /var/www/html/emerson_$2/data/temp/temp_unit.txt
