051d8617d59173d00d6d0d327c9f8d859a665f3a
George Zachos <cse32439@cs.uoi.gr>
2015-12-28 20:31:29 +0200

    Updated generate_input_data.sh

--- generate_input_data.sh
+++ generate_input_data.sh
@@ -26,42 +26,56 @@
 # (Parameters: $1 -> IP address part, $2 -> Emerson unit No.) 
 retrieve_data () {
 	ATTEMPTS=0
+	CURR_STATUS_FILE="${WEBSITEPATH}/emerson_${2}/data/curr_status.txt"
+	TEMP_UNIT_FILE="${WEBSITEPATH}/emerson_${2}/data/temp/temp_unit.txt"
+	HUM_UNIT_FILE="${WEBSITEPATH}/emerson_${2}/data/hum/hum_unit.txt"
+	TEMP_SYS_FILE="${WEBSITEPATH}/emerson_${2}/data/temp/temp_sys.txt"
+	HUM_SYS_FILE="${WEBSITEPATH}/emerson_${2}/data/hum/hum_sys.txt"
+	EVENT_LOG_FILE="${WEBSITEPATH}/emerson_${2}/data/event_log.txt"
+	local EC=0
+	FLAG=0
 	while [ "${ATTEMPTS}" -le "4" ]
 	do
 		EC=0
-		wget http://192.168.254.${1}/index.html --timeout=2 --tries=1 -O ${WEBSITEPATH}/emerson_${2}/data/curr_status.txt >> /dev/null 2>&1
+		FLAG=0
+		wget http://192.168.254.${1}/index.html --timeout=2 --tries=1 -O ${CURR_STATUS_FILE} >> /dev/null 2>&1
 		((EC += $?))
-		wget http://192.168.254.${1}/tmp/1102.xml --timeout=2 --tries=1 -O ${WEBSITEPATH}/emerson_${2}/data/temp/temp_unit.txt >> /dev/null 2>&1
+		wget http://192.168.254.${1}/tmp/1102.xml --timeout=2 --tries=1 -O ${TEMP_UNIT_FILE} >> /dev/null 2>&1
 		((EC += $?))
-		wget http://192.168.254.${1}/tmp/1103.xml --timeout=2 --tries=1 -O ${WEBSITEPATH}/emerson_${2}/data/hum/hum_unit.txt >> /dev/null 2>&1
+		wget http://192.168.254.${1}/tmp/1103.xml --timeout=2 --tries=1 -O ${HUM_UNIT_FILE} >> /dev/null 2>&1
 		((EC += $?))
-		wget http://192.168.254.${1}/tmp/1104.xml --timeout=2 --tries=1 -O ${WEBSITEPATH}/emerson_${2}/data/temp/temp_sys.txt >> /dev/null 2>&1
+		wget http://192.168.254.${1}/tmp/1104.xml --timeout=2 --tries=1 -O ${TEMP_SYS_FILE} >> /dev/null 2>&1
 		((EC += $?))
-		wget http://192.168.254.${1}/tmp/1105.xml --timeout=2 --tries=1 -O ${WEBSITEPATH}/emerson_${2}/data/hum/hum_sys.txt >> /dev/null 2>&1
+		wget http://192.168.254.${1}/tmp/1105.xml --timeout=2 --tries=1 -O ${HUM_SYS_FILE} >> /dev/null 2>&1
 		((EC += $?))
-		wget http://192.168.254.${1}/tmp/statusreport.xml --timeout=2 --tries=1 -O ${WEBSITEPATH}/emerson_${2}/data/event_log.txt >> /dev/null 2>&1
+		wget http://192.168.254.${1}/tmp/statusreport.xml --timeout=2 --tries=1 -O ${EVENT_LOG_FILE} >> /dev/null 2>&1
 		((EC += $?))
-		MAIN_CONTENT=$(cat ${WEBSITEPATH}/emerson_${2}/data/curr_status.txt)
-		if [ "${EC}" -eq "0" ] || [ -n "${MAIN_CONTENT}" ]
+		if [ "${EC}" -eq "0" ] && [ -s "${CURR_STATUS_FILE}" ] && [ -s "${TEMP_UNIT_FILE}" ] && [ -s "${HUM_UNIT_FILE}" ] && [ -s "${TEMP_SYS_FILE}" ] && [ -s "${HUM_SYS_FILE}" ] && [ -s "${EVENT_LOG_FILE}" ]
 		then
-			break
+			FLAG=2
+			LINE_COUNT=$(cat ${CURR_STATUS_FILE} ${TEMP_UNIT_FILE} ${HUM_UNIT_FILE} ${TEMP_SYS_FILE} ${HUM_SYS_FILE} | wc -l)
+			if [ "${LINE_COUNT}" -eq "3935" ]
+			then
+				FLAG=1
+				break
+			else
+				echo "[ $(date -R) ] Input data for Emerson unit #${2} were incompletely retrieved. Attempt: #${ATTEMPTS} [WARN]" >> ${GLB_LOGFILE}
+			fi
 		fi
 		((ATTEMPTS += 1))
 	done
 	
-	if [ "${EC}" -ne "0" ]
+	if [ "${FLAG}" -eq "0" ]
 	then
 		echo "[ $(date -R) ] Input data for Emerson unit #${2} were NOT successfully retrieved [FAIL]" >> ${GLB_LOGFILE}
 		return 2
-	fi
-
-	if [ -z "${MAIN_CONTENT}" ]
+	elif [ "${FLAG}" -eq "1" ]
 	then
-		echo "[ $(date -R) ] Input data for Emerson unit #${2} were successfully retrieved but one or more files are empty [FAIL]" >> ${GLB_LOGFILE}
-		return 3
-	else
 		echo "[ $(date -R) ] Input data for Emerson unit #${2} were successfully retrieved" >> ${GLB_LOGFILE}
 		return 0
+	else
+		echo "[ $(date -R) ] Input data for Emerson unit #${2} were retrieved but some files are corrupt [FAIL]" >> ${GLB_LOGFILE}
+		return 3
 	fi
 }
 
@@ -78,14 +92,20 @@ main () {
 	((EC += $?))
 	ping -c 3 192.168.254.2 >> /dev/null 2>&1
 	((EC += $?))
-	if [ "${EC}" -eq "0" ]
+	if [ "${EC}" -ne "0" ]
 	then
-		retrieve_data 1 4
-		retrieve_data 2 3
-	else
 		echo "[ $(date -R) ] Cannot connect to host with IP: 192.168.254.{1,2} and retrieve input data [FAIL]" >> ${GLB_LOGFILE}
 		exit 1
 	fi
+	retrieve_data 1 4
+	((EC += $?))
+	retrieve_data 2 3
+	((EC += $?))
+	if [ "${EC}" -ne "0" ]
+	then
+		exit 2
+	fi
+	exit 0
 }
 
 
