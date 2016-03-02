ca5ed9dece348d507d9e86e030e6189b9cddd627
George Zachos <cse32439@cs.uoi.gr>
2016-03-02 23:54:04 +0200

    Updated copyright years

--- update_data.sh
+++ update_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #+-----------------------------------------------------------------------+
-#|                 Copyright (C) 2015 George Z. Zachos                   |
+#|              Copyright (C) 2015-2016 George Z. Zachos                 |
 #+-----------------------------------------------------------------------+
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
